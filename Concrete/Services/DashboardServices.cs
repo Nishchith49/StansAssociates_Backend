83b8f516a884eeab8eeeaf7775b973cc29001423
Nishchith49 <nishchithdn5551@gmail.com>
2025-08-11 17:00:05 +0530

    Added role based dashboard

--- Concrete/Services/DashboardServices.cs
+++ Concrete/Services/DashboardServices.cs
@@ -9,22 +9,38 @@ namespace StansAssociates_Backend.Concrete.Services
     public class DashboardServices : IDashboardServices
     {
         private readonly StansassociatesAntonyContext _context;
+        private readonly ICurrentUserServices _currentUser;
 
-        public DashboardServices(StansassociatesAntonyContext context)
+        public DashboardServices(StansassociatesAntonyContext context, ICurrentUserServices currentUser)
         {
             _context = context;
+            _currentUser = currentUser;
         }
 
 
         public async Task<ServiceResponse<GetDashboardModel>> GetDashboard()
         {
+            var isAdmin = _currentUser.IsAdmin;
+            var schoolId = _currentUser.UserId;
             var dashboard = new GetDashboardModel
             {
-                TotalStudents = await _context.Students.CountAsync(),
-                TotalStaffs = await _context.Users.Where(x => x.UserRoles.Any(x => x.RoleId == 2)).CountAsync(),
-                TotalRoutes = await _context.Routes.CountAsync(),
-                TotalEarnings = await _context.StudentFeesHistories.SumAsync(x => x.Amount),
-                TotalEarningsThisYear = await _context.StudentFeesHistories.Where(x => x.PaidDate.Year == DateTime.Now.Year).SumAsync(x => x.Amount),
+                TotalStudents = await _context.Students
+                                              .Where(x => isAdmin || x.SchoolId == schoolId)
+                                              .CountAsync(),
+                TotalStaffs = await _context.Users
+                                            .Where(x => isAdmin || x.SchoolId == schoolId)
+                                            .Where(x => x.UserRoles.Any(x => x.RoleId == 2))
+                                            .CountAsync(),
+                TotalRoutes = await _context.Routes
+                                            .Where(x => isAdmin || x.SchoolId == schoolId)
+                                            .CountAsync(),
+                TotalEarnings = await _context.StudentFeesHistories
+                                              .Where(x => isAdmin || x.Student.SchoolId == schoolId)
+                                              .SumAsync(x => x.Amount),
+                TotalEarningsThisYear = await _context.StudentFeesHistories
+                                                      .Where(x => isAdmin || x.Student.SchoolId == schoolId)
+                                                      .Where(x => x.PaidDate.Year == DateTime.Now.Year)
+                                                      .SumAsync(x => x.Amount),
             };
             return new(ResponseConstants.Success, 200, dashboard);
         }
